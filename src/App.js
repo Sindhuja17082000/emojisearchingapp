0e18593086b63edfdeeeb78fbea8c40a6f60f7dd
Sindhuja17082000 <118460131+Sindhuja17082000@users.noreply.github.com>
2023-01-25 16:06:33 +0530

    initial commit
    emoji app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect, useState} from 'react'
+import EmojiData from './Emoji.json';
 
-function App() {
+
+const App = () => {
+  const [search,setSearch] = useState("");
+  const [data,setData] = useState([]);
+
+  useEffect(()=>{
+      const newData = EmojiData.filter(emoji => emoji.title.toLowerCase().includes(search.toLowerCase()));
+      setData(newData);
+  },[search])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center >
+        <h1 style={{color:'Brown'}}>  Welcome To Emoji Search  <span>
+          <img src='https://blog.hubspot.com/hubfs/00-Blog_Thinkstock_Images/emoji-marketing.png'/>
+          </span>
+          
+
+          </h1>
+        <input  size="30" type="text" value={search} style={{color:'Black'}} onChange={(e) => setSearch(e.target.value)} />
+        </center>
+        {data.map(emoji =>
+          <div className="card" key={emoji.title}>
+          <div className="card-body" onClick={() => {navigator.clipboard.writeText(emoji.symbol);alert("Emoji Copied")}}>
+            {emoji.symbol} {emoji.title}
+          </div>
+        </div>
+        )}
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
